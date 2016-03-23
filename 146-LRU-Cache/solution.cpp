1850046a94f1d119dd4871850a8654ce8ae8256b
Eli Gao <refraction222@gmail.com>
2016-03-23 18:27:09 +0800

    [Accepted]committed by LeetCode Extension

--- 146-LRU-Cache/solution.cpp
+++ 146-LRU-Cache/solution.cpp
@@ -6,43 +6,32 @@ private:
 	int capacity;
 
 public:
-	LRUCache(int capacity) :
-	capacity(capacity)
-	{
-		
-	}
+	LRUCache(int capacity) :capacity(capacity)	{}
 
 	int get(int key) {
 		auto it_map = hash_map.find(key);
-		auto it_queue = queue_list.end();
-		int val=-1;
-		if (it_map != hash_map.end())	//if key found
-		{
-			it_queue = it_map->second;
-			val = it_queue->second;				
-			queue_list.push_back(*it_queue);	//copy the elem to the beginning of queue
-			queue_list.erase(it_queue);			//remove old elem
-			hash_map[key] = --queue_list.end();	//update map to the elem
-		}
-		return val;
+		if (it_map == hash_map.end())	return -1;//if key not found
+		queue_list.push_front(*it_map->second);	//copy the elem to the beginning of queue
+		queue_list.erase(it_map->second);			//remove old elem
+		it_map->second = queue_list.begin();
+		return it_map->second->second;
 	}
 
 	void set(int key, int value) {
 		auto it_map = hash_map.find(key);
 		if (it_map != hash_map.end())	//if key found
 		{
-			auto it_queue = it_map->second;	
-			queue_list.erase(it_queue);	//erase the old key
+			queue_list.erase(it_map->second);	//erase the old key
 		}
 		else	//key not found,  
 		{
 			if (queue_list.size() == capacity)	//if already full
 			{
-				hash_map.erase(hash_map.find(queue_list.front().first));
-				queue_list.pop_front(); //deque the last recent used key
+				hash_map.erase(queue_list.rbegin()->first);
+				queue_list.pop_back(); //deque the last recent used key
 			}
 		}
-		queue_list.push_back(make_pair(key, value)); 
-		hash_map[key] = --queue_list.end();	//enque new key
+		queue_list.push_front(make_pair(key, value)); 
+		hash_map[key] = queue_list.begin();	//enque new key
 	}
 };
